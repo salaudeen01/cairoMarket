100a1d4742036c0a5ca3ca9eac8442b7ede53f31
Hameedahlawal <lawalhameedah87@gmail.com>
2021-01-14 04:55:42 -0800

    hameedah

--- src/myProject/myProject.js
+++ src/myProject/myProject.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import images from '../Images/ata.jpg'
 
  class Project extends Component {
     render() {
@@ -12,7 +11,7 @@ import images from '../Images/ata.jpg'
                 alignItems:'center',
                 alignContent:'center'}}
                 >
-            <div className="container" style={{padding:200}}> 
+            <div className="container" style={{padding:200, marginLeft:50}}> 
                 <div class="card text-center " style={{width:1000,height:300 }}>
                     <div class="card-body">
                         <h1 class="card-title">Forgot Your Password <span className="fa fa-comment-o"/></h1>
@@ -23,9 +22,9 @@ import images from '../Images/ata.jpg'
                         </h3>
                         <br/>
                         <form className="form-group row g-3" style={{width:'80%' ,height:'auto',margin:'0px auto'}}>
-                            <input style={{width:500,height:50}} type="email,number" class=" form-control" 
+                            <input style={{width:500,height:50, fontSize:15}} type="email,number" class=" form-control" 
                             placeholder="Your email address or phone number"/> 
-                            <a href="#" style={{width:200}} class="btn btn-primary "><h4>Submit</h4></a>
+                            <a href="#" style={{width:200, fontSize:20}} class="btn btn-primary ">Submit</a>
                         </form>
                     </div>
                 </div>
